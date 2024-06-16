e06202909af7b7fbcdc904ca4031acd7af6f69f4
NikolayDvoinikh <108171672+NikolayDvoinikh@users.noreply.github.com>
2024-06-16 13:34:46 +0300

    add dashboard component

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,67 +1,67 @@
-// import AnalyticsDashboard from "@/components/AnalyticsDashboard";
+import AnalyticsDashboard from "@/components/AnalyticsDashboard";
 import { getDate } from "@/utils";
 import { analytics } from "@/utils/analytics";
 
 const Page = async () => {
-  // const TRACKING_DAYS = 7;
+  const TRACKING_DAYS = 7;
 
-  // const pageviews = await analytics.retrieveDays("pageview", 7);
+  const pageviews = await analytics.retrieveDays("pageview", 7);
 
-  // const totalPageViews = pageviews.reduce(
-  //   (acc, curr) =>
-  //     acc + curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0),
-  //   0
-  // );
+  const totalPageViews = pageviews.reduce(
+    (acc, curr) =>
+      acc + curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0),
+    0
+  );
 
-  // const avgVisitorsPerDay = (totalPageViews / TRACKING_DAYS).toFixed(1);
+  const avgVisitorsPerDay = (totalPageViews / TRACKING_DAYS).toFixed(1);
 
-  // const amtVisitorsToday = pageviews
-  //   .filter((ev) => ev.date === getDate())
-  //   .reduce(
-  //     (acc, curr) =>
-  //       acc +
-  //       curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0),
-  //     0
-  //   );
+  const amtVisitorsToday = pageviews
+    .filter((ev) => ev.date === getDate())
+    .reduce(
+      (acc, curr) =>
+        acc +
+        curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0),
+      0
+    );
 
-  // const topCountriesMap = new Map<string, number>();
+  const topCountriesMap = new Map<string, number>();
 
-  // for (let i = 0; i < pageviews.length; i++) {
-  //   const day = pageviews[i];
-  //   if (!day) continue;
+  for (let i = 0; i < pageviews.length; i++) {
+    const day = pageviews[i];
+    if (!day) continue;
 
-  //   for (let j = 0; j < day.events.length; j++) {
-  //     const event = day.events[j];
-  //     if (!event) continue;
-  //     const key = Object.keys(event)[0]!;
-  //     const value = Object.values(event)[0]!;
-  //     const parsedKey = JSON.parse(key);
-  //     const country = parsedKey?.country;
+    for (let j = 0; j < day.events.length; j++) {
+      const event = day.events[j];
+      if (!event) continue;
+      const key = Object.keys(event)[0]!;
+      const value = Object.values(event)[0]!;
+      const parsedKey = JSON.parse(key);
+      const country = parsedKey?.country;
 
-  //     if (country) {
-  //       if (topCountriesMap.has(country)) {
-  //         const prevValue = topCountriesMap.get(country)!;
-  //         topCountriesMap.set(country, prevValue + value);
-  //       } else {
-  //         topCountriesMap.set(country, value);
-  //       }
-  //     }
-  //   }
-  // }
+      if (country) {
+        if (topCountriesMap.has(country)) {
+          const prevValue = topCountriesMap.get(country)!;
+          topCountriesMap.set(country, prevValue + value);
+        } else {
+          topCountriesMap.set(country, value);
+        }
+      }
+    }
+  }
 
-  // const topCountries = [...topCountriesMap.entries()]
-  //   .sort((a, b) => (a > b ? -1 : 1))
-  //   .slice(0, 5);
+  const topCountries = [...topCountriesMap.entries()]
+    .sort((a, b) => (a[1] > b[1] ? -1 : 1))
+    .slice(0, 5);
 
   return (
     <div className="min-h-screen w-full py-12 flex justify-center items-center">
       <div className="relative w-full max-w-6xl mx-auto text-white">
-        {/* <AnalyticsDashboard
+        <AnalyticsDashboard
           avgVisitorsPerDay={avgVisitorsPerDay}
           amtVisitorsToday={amtVisitorsToday}
           timeseriesPageviews={pageviews}
           topCountries={topCountries}
-        /> */}
+        />
       </div>
     </div>
   );
