de52da4b1bce845ac6401374f0098fea22e3c554
NikolayDvoinikh <108171672+NikolayDvoinikh@users.noreply.github.com>
2024-06-16 16:09:29 +0300

    tets

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -3,35 +3,35 @@ import { getDate } from "@/utils";
 import { analytics } from "@/utils/analytics";
 
 const Page = async () => {
-  const TRACKING_DAYS = 7;
+  // const TRACKING_DAYS = 7;
 
-  const pageviews = await analytics.retrieveDays("pageview", 7);
+  // const pageviews = await analytics.retrieveDays("pageview", 7);
 
-  const totalPageViews = pageviews.reduce(
-    (acc, curr) =>
-      acc + curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0),
-    0
-  );
+  // const totalPageViews = pageviews.reduce(
+  //   (acc, curr) =>
+  //     acc + curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0),
+  //   0
+  // );
 
-  const avgVisitorsPerDay = (totalPageViews / TRACKING_DAYS).toFixed(1);
+  // const avgVisitorsPerDay = (totalPageViews / TRACKING_DAYS).toFixed(1);
 
-  const amtVisitorsToday = pageviews
-    .filter((ev) => ev.date === getDate())
-    .reduce(
-      (acc, curr) =>
-        acc +
-        curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0),
-      0
-    );
+  // const amtVisitorsToday = pageviews
+  //   .filter((ev) => ev.date === getDate())
+  //   .reduce(
+  //     (acc, curr) =>
+  //       acc +
+  //       curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0),
+  //     0
+  //   );
 
     return (
     <div className="min-h-screen w-full py-12 flex justify-center items-center">
       <div className="relative w-full max-w-6xl mx-auto text-white">
-        <AnalyticsDashboard
+        {/* <AnalyticsDashboard
           avgVisitorsPerDay={avgVisitorsPerDay}
           amtVisitorsToday={amtVisitorsToday}
           timeseriesPageviews={pageviews}
-        />
+        /> */}
       </div>
     </div>
   );
